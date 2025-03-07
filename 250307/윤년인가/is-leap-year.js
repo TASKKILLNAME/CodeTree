285ba52df1968a7a0658ffde81a131237faea7c7
202244067 <dongjunjwa4020@naver.com>
2025-03-07 14:30:05 +0900

    Title: 윤년인가, Time: 187ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 250307/윤년인가/is-leap-year.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim();
+
+let year = Number(input);
+
+if (year % 4 == 0 && year % 100 !== 0) {
+    console.log("true");
+} else if (year % 100 ==0 && year % 400 !== 0) {
+    console.log("false");
+}
+
