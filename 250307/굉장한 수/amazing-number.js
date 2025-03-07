615d19e5953b1007d89da52031b9adbce86bba1c
202244067 <dongjunjwa4020@naver.com>
2025-03-07 14:07:54 +0900

    Title: 굉장한 수, Time: 182ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 250307/굉장한 수/amazing-number.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+
+let input = fs.readFileSync(0).toString().trim();
+
+let a = Number(input);
+
+if ( a % 2 == 1 && a % 3 == 0 || a % 2 == 0 && a % 5 == 0) {
+    console.log("true");
+}
+else {
+    console.log("false");
+}
