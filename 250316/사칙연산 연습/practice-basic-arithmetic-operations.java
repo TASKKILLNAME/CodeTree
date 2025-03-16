21544dbfa60b640b73699bf373b933be7088e7d1
202244067 <dongjunjwa4020@naver.com>
2025-03-16 16:45:36 +0900

    Title: 사칙연산 연습, Time: 90ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 250316/사칙연산 연습/practice-basic-arithmetic-operations.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 30;
+        int c = a + b;
+
+        int result1 = c / 8; 
+        int result2 = c %= 8;
+
+        System.out.println(result1);
+        System.out.print(result2);
+    }
+}
